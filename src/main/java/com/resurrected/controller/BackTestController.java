20a66a52ffd89e2957013b4a9cbb02b479a25941
octaviofarinia <octaviofarinia@gmail.com>
2022-02-15 20:24:12 -0300

    esqueleto pagina de desarrollo del back para metodos de listar productos y filtros

--- src/main/java/com/resurrected/controller/BackTestController.java
+++ src/main/java/com/resurrected/controller/BackTestController.java
@@ -1,8 +1,10 @@
 package com.resurrected.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/back")
@@ -11,6 +13,16 @@ public class BackTestController {
 	@GetMapping("/inicio")
 	public String inicio() {
 		return "back/inicio.html";
+	}
+	
+	@GetMapping("/productos")
+	public String productos(ModelMap model, @RequestParam(required = false) String status) {
+		
+		//TODO: listar todos los productos => hacer paginable => agregar filtros.
+		model.put("status", status);
+		
+		
+		return "back/productos.html";
 	}
 	
 }
