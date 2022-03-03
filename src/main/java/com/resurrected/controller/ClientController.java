4337174a3a88c570d9cbe04140018f0565c544c7
Chulo <chulogiamportone@gmail.com>
2022-03-02 23:22:17 -0700

    Arreglo y Limpieza de Index y Producto

--- src/main/java/com/resurrected/controller/ClientController.java
+++ src/main/java/com/resurrected/controller/ClientController.java
@@ -35,6 +35,15 @@ public class ClientController {
 	public String registrate() {
 		return "registrate.html";
 	}
+	@GetMapping("/producto")
+	public String producto() {
+		return "product.html";
+	}
+	@GetMapping("/inicio")
+	public String inicio() {
+		return "inicio.html";
+	}
+
 
 	@PreAuthorize("hasAnyRole('ROLE_USER_CLIENT','ROLE_USER_ADMIN')")
 	@GetMapping("/perfil")
